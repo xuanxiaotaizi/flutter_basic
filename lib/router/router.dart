11b77f80921c46edbdf6dcf35eb75da66e9e00f9
wanggang <1211239683@qq.com>
2020-07-22 18:14:44 +0800

    自动路由

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,9 +1,10 @@
 import 'package:appdemo/pages/user/user_detail.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
-    MaterialRoute(page: UserDetail),
+    CustomRoute(page: UserDetail, transitionsBuilder: TransitionsBuilders.fadeIn)
   ],
 )
 class $Router {}
